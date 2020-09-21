b7974b284595640c3f568a97cdfe59b76883a6e6
Andrea Censi <acensi@ethz.ch>
2020-09-21 21:36:58 +0200

    WIP

--- wrappers.py
+++ wrappers.py
@@ -1,7 +1,11 @@
 
 import numpy as np
 
-class DTPytorchWrapper():
+import cv2
+
+
+
+class DTPytorchWrapper:
     def __init__(self, shape=(120, 160, 3)):
         self.shape = shape
         self.transposed_shape = (shape[2], shape[0], shape[1])
@@ -9,8 +13,6 @@ class DTPytorchWrapper():
     def preprocess(self, obs):
         #from PIL import Image
         #return np.array(Image.fromarray(obs).resize(self.shape[0:2])).transpose(2, 0, 1)
-        
-        import cv2
         obs = cv2.resize(obs, self.shape[0:2])
         # NOTICE: OpenCV changes the order of the channels !!!
         obs = cv2.cvtColor(obs, cv2.COLOR_BGR2RGB)
